4b4047cc27b5885a99a34e92f5755338a5531355
Daniel Palmer <d.m.palmer@wustl.edu>
2025-11-11 15:36:54 -0600

    created test server subdir

---
+++ lab3server/target/debug/deps/lab3server-c1fcb0c2793e480e.d
@@ -0,0 +1,5 @@
+/home/compute/d.m.palmer/cse5402/lab3/lab3server/target/debug/deps/lab3server-c1fcb0c2793e480e: src/main.rs
+
+/home/compute/d.m.palmer/cse5402/lab3/lab3server/target/debug/deps/lab3server-c1fcb0c2793e480e.d: src/main.rs
+
+src/main.rs:
---
+++ lab3server/target/debug/lab3server.d
@@ -0,0 +1,1 @@
+/home/compute/d.m.palmer/cse5402/lab3/lab3server/target/debug/lab3server: /home/compute/d.m.palmer/cse5402/lab3/lab3server/src/main.rs
